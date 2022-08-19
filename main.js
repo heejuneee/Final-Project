20005d504ac789e8216798ebd049cad65f8400e3
박희준 <107246410+heejuneee@users.noreply.github.com>
2022-08-19 10:49:01 +0900

    Update main.js

--- main.js
+++ main.js
@@ -44,98 +44,98 @@ async function main() {
         }
 
     });  
-    // 발송할 메일의 컨테츠 정보
-    let info = await transporter.sendMail({
+//     // 발송할 메일의 컨테츠 정보
+//     let info = await transporter.sendMail({
 
-          from : "primejune@naver.com"         // @details 보내는 사람 메일주소( user 메일주소와 일치해야함 )
+//           from : "primejune@naver.com"         // @details 보내는 사람 메일주소( user 메일주소와 일치해야함 )
 
-        , to :    "primejune@naver.com"            // @details 받는이(들) 메일주소
+//         , to :    "primejune@naver.com"            // @details 받는이(들) 메일주소
 
-        , subject : "SYT 인증번호 입니다. ✔"                           // @details 메일주소
+//         , subject : "SYT 인증번호 입니다. ✔"                           // @details 메일주소
 
-        // , text : "HTML 속성이 활성화 되어 있으면 비활성화 됨"         // @details 메일 내용 
+//         // , text : "HTML 속성이 활성화 되어 있으면 비활성화 됨"         // @details 메일 내용 
       
 
-        , html :  "인증번호는 " + authNum + "입니다."            // @details 메일 내용(HTML)
+//         , html :  "인증번호는 " + authNum + "입니다."            // @details 메일 내용(HTML)
 
-    });
-
-
-
-    console.log("Message sent : %s", info.messageId);
-
-}
-
-
-// @breif main() 함수를 실행
-
-main().catch( console.error );
+//     });
 
 
 
-const app = express();
+//     console.log("Message sent : %s", info.messageId);
 
-app.set('port', process.env.PORT || 8001);
-app.set('view engine', 'html');
-nunjucks.configure('views', {
+// }
+
+
+// // @breif main() 함수를 실행
+
+// main().catch( console.error );
+
+
+
+// const app = express();
+
+// app.set('port', process.env.PORT || 8001);
+// app.set('view engine', 'html');
+// nunjucks.configure('views', {
   
-  express: app,
-  watch: true,
-});
+//   express: app,
+//   watch: true,
+// });
 
-sequelize.sync({ force: false })
+// sequelize.sync({ force: false })
 
-  .then(() => {
-    console.log('데이터베이스 연결 성공');
-  })
-  .catch((err) => {
-    console.error(err);
-  });
+//   .then(() => {
+//     console.log('데이터베이스 연결 성공');
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//   });
 
-app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/img', express.static(path.join(__dirname, 'uploads')));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(session({
-  resave: false,
-  saveUninitialized: false,
-  secret: process.env.COOKIE_SECRET,
-  cookie: {
-    httpOnly: true,
-    secure: false,
-  },
-}));
-
-
+// app.use(morgan('dev'));
+// app.use(express.static(path.join(__dirname, 'public')));
+// app.use('/img', express.static(path.join(__dirname, 'uploads')));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser(process.env.COOKIE_SECRET));
+// app.use(session({
+//   resave: false,
+//   saveUninitialized: false,
+//   secret: process.env.COOKIE_SECRET,
+//   cookie: {
+//     httpOnly: true,
+//     secure: false,
+//   },
+// }));
 
 
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use('/', pageRouter);
-app.use('/auth', authRouter);
-app.use('/post', postRouter);
-app.use('/user', userRouter);
 
 
-app.use((req, res, next) => {
-  const error =  new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
-  error.status = 404;
-  next(error);
-});
+// app.use(passport.initialize());
+// app.use(passport.session());
 
-app.use((err, req, res, next) => {
-  res.locals.message = err.message;
-  res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
-  res.status(err.status || 500);
-  res.render('error');
-});
+// app.use('/', pageRouter);
+// app.use('/auth', authRouter);
+// app.use('/post', postRouter);
+// app.use('/user', userRouter);
 
-app.listen(app.get('port'), () => {
-  console.log(app.get('port'), '번 포트에서 대기중');
-});
+
+// app.use((req, res, next) => {
+//   const error =  new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+//   error.status = 404;
+//   next(error);
+// });
+
+// app.use((err, req, res, next) => {
+//   res.locals.message = err.message;
+//   res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
+
+// app.listen(app.get('port'), () => {
+//   console.log(app.get('port'), '번 포트에서 대기중');
+// });
 
 
 
