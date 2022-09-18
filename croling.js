ab50ac8c50ae2dea3ba799b11c45b9979a76c8f5
박희준 <107246410+heejuneee@users.noreply.github.com>
2022-09-19 08:34:33 +0900

    Update croling.js

--- croling.js
+++ croling.js
@@ -10,25 +10,25 @@ const getHtml = async () => {
   }
 };
 
-// getHtml()
-//   .then(html => {
-//     let ulList = [];
-//     const $ = cheerio.load(html.data);
-//     const $bodyList = $("div.headline-list ul").children("li.section02");
+getHtml()
+  .then(html => {
+    let ulList = [];
+    const $ = cheerio.load(html.data);
+    const $bodyList = $("div.headline-list ul").children("li.section02");
 
-//     $bodyList.each(function(i, elem) {
-//       ulList[i] = {
-//           title: $(this).find('strong.news-tl a').text(),
-//           url: $(this).find('strong.news-tl a').attr('href'),
-//           image_url: $(this).find('p.poto a img').attr('src'),
-//           image_alt: $(this).find('p.poto a img').attr('alt'),
-//           summary: $(this).find('p.lead').text().slice(0, -11),
-//           date: $(this).find('span.p-time').text()
-//       };
-//     });
+    $bodyList.each(function(i, elem) {
+      ulList[i] = {
+          title: $(this).find('strong.news-tl a').text(),
+          url: $(this).find('strong.news-tl a').attr('href'),
+          image_url: $(this).find('p.poto a img').attr('src'),
+          image_alt: $(this).find('p.poto a img').attr('alt'),
+          summary: $(this).find('p.lead').text().slice(0, -11),
+          date: $(this).find('span.p-time').text()
+      };
+    });
 
-//     const data = ulList.filter(n => n.title);
-//     return data;
-//   })
-//   .then(res => log(res))
+    const data = ulList.filter(n => n.title);
+    return data;
+  })
+  .then(res => log(res))
 
